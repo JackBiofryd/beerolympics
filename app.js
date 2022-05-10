45bfcd2570133d6364f98615a606685d8a54323f
unknown <jackbiofryd@gmail.com>
2022-05-10 20:48:35 +0200

    added backedn url

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'http://127.0.0.1:5000';
+const BACKEND_URL = 'https://beerolympicss.herokuapp.com/';
 
 const modals = document.querySelectorAll('.modal');
 modals.forEach(modal => modal.addEventListener('click', hideModal));
